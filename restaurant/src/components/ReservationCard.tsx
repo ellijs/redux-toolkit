b7a6ff69330a41c8685f9f9d050c7ede05e7e926
ellijs <ellijs@hotmail.com>
2022-01-12 21:54:50 -0500

    prettier

--- restaurant/src/components/ReservationCard.tsx
+++ restaurant/src/components/ReservationCard.tsx
@@ -14,14 +14,16 @@ function ReservationCard({ name, index }: ReservationCardType) {
   const dispatch = useDispatch();
   return (
     <div
-      className="reservation-card-container"
-      onClick={()=>{
-        dispatch(removeReservation(index))
-        dispatch(addCustomer({ 
-          id: uuid(),
-          name, 
-          food: []
-        }))
+      className='reservation-card-container'
+      onClick={() => {
+        dispatch(removeReservation(index));
+        dispatch(
+          addCustomer({
+            id: uuid(),
+            name,
+            food: [],
+          })
+        );
       }}
     >
       <p>{name}</p>
